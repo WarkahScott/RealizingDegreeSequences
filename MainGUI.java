74775b72c148d8529384f679a959607b07e5b21c
Warkah Scott <warkahscott@gmail.com>
2019-02-14 02:12:12 -0500

    Integrated file input

--- MainGUI.java
+++ MainGUI.java
@@ -1,11 +1,14 @@
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.Scanner;
 import java.util.Set;
 
 import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.geometry.Insets;
-import javafx.geometry.Orientation;
 import javafx.geometry.Pos;
 import javafx.geometry.Rectangle2D;
 import javafx.scene.Group;
@@ -15,7 +18,6 @@ import javafx.scene.control.*;
 import javafx.scene.image.*;
 import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.HBox;
-import javafx.scene.layout.TilePane;
 import javafx.scene.layout.VBox;
 import javafx.stage.FileChooser;
 import javafx.stage.FileChooser.ExtensionFilter;
@@ -93,11 +95,11 @@ public class MainGUI extends Application
 										try
 										{
 											int num = Integer.parseInt(parameter.getText());
-											manualEntryGraphs(window, input, chosen, num);
+											manualEntryGraphs(window, input.getText(), chosen, num);
 										}
 										catch(NumberFormatException x)
 										{
-											manualEntryGraphs(window, input, chosen, null);
+											manualEntryGraphs(window, input.getText(), chosen, null);
 										}
 								  	});
 		
@@ -110,13 +112,13 @@ public class MainGUI extends Application
 		return new Scene(v1, 525, 200);
 	}
 	
-	private static void manualEntryGraphs(Stage owner, TextField input, Set<String> chosen, Integer parameter)
+	private static void manualEntryGraphs(Stage owner, String input, Set<String> chosen, Integer parameter)
 	{
 		HavelHakimi2 network;
 		if(parameter == null)
-			network = new HavelHakimi2(input.getText());
+			network = new HavelHakimi2(input);
 		else
-			network = new HavelHakimi2(input.getText(), parameter.intValue());
+			network = new HavelHakimi2(input, parameter.intValue());
 		ArrayList<Group> max = network.getMaxHH();
 		ArrayList<Group> min = network.getMinHH();
 		ArrayList<Group> uni = network.getUniHH();
@@ -161,7 +163,6 @@ public class MainGUI extends Application
 			view.sizeToScene();
 			view.show();
 		}
-		input.setText(network.getOutput());
 	}
 	
 	private static Scene FileInputScene(Stage window)
@@ -194,6 +195,8 @@ public class MainGUI extends Application
 									}
 								});
 		
+		
+		
 		RadioButton r1 = new RadioButton("Single-Line");
 		RadioButton r2 = new RadioButton("File");
 		
@@ -207,77 +210,84 @@ public class MainGUI extends Application
 																					window.setScene(ManualInputScene(window));
 																					window.show();
 																				 });
-		
-		CheckBox max = new CheckBox("Max");
-			max.setAllowIndeterminate(false);
-		CheckBox min = new CheckBox("Min");
-			min.setAllowIndeterminate(false);
-		CheckBox uni = new CheckBox("Uniform");
-			uni.setAllowIndeterminate(false);
-		CheckBox pro = new CheckBox("Probabilistic");
-			pro.setAllowIndeterminate(false);
-		CheckBox par = new CheckBox("Parameterized");
-			par.setAllowIndeterminate(false);
-			par.setDisable(true);
-	
+		CheckBox max = new CheckBox("Max");                     
+			max.setAllowIndeterminate(false);                   
+		CheckBox min = new CheckBox("Min");                     
+			min.setAllowIndeterminate(false);                   
+		CheckBox uni = new CheckBox("Uniform");                 
+			uni.setAllowIndeterminate(false);                   
+		CheckBox pro = new CheckBox("Probabilistic");           
+			pro.setAllowIndeterminate(false);                   
+		CheckBox par = new CheckBox("Paramaterized");           
+			par.setAllowIndeterminate(false);                   
+			                                                    
+		TextField parameter = new TextField();                  
+		parameter.setPrefSize(50, 15);                          
+		                                                     
 		HBox checkboxes = new HBox(20, max, min, uni, pro, par);
-			checkboxes.setAlignment(Pos.CENTER);
+			checkboxes.setAlignment(Pos.CENTER);                
+		HBox selection = new HBox(5, checkboxes, parameter);    
+		selection.setAlignment(Pos.CENTER); 
+		
+		generate.setOnAction(e -> 	{
+										Set<String> chosen = new HashSet<String>();
+										for(Node c : checkboxes.getChildren())
+											if(((CheckBox) c).isSelected())
+												chosen.add(((CheckBox) c).getText());
+										try
+										{
+											int num = Integer.parseInt(parameter.getText());
+											fileEntryGraphs(window, input, chosen, num);
+										}
+										catch(NumberFormatException x)
+										{
+											fileEntryGraphs(window, input, chosen, null);
+										}
+								  	});
 		
 		HBox h1 = new HBox(input, select);
 		h1.setAlignment(Pos.CENTER);
 		HBox h2 = new HBox(20, r1, r2);
 		h2.setAlignment(Pos.CENTER);
 		
-		VBox v1 = new VBox(20, prompt, h1, h2, checkboxes, generate);
+		VBox v1 = new VBox(15, prompt, h1, h2, selection, generate);
 		v1.setAlignment(Pos.CENTER);
 		
-		return new Scene(v1, 450, 200);
+		return new Scene(v1, 525, 200);
 	}
 	
-	private static void fileEntryGraphs(Stage owner, TextField input, Set<String> chosen)
+	private static void fileEntryGraphs(Stage owner, TextField input, Set<String> chosen, Integer parameter)
 	{
-		HavelHakimi2 network = new HavelHakimi2(input.getText());
-		ArrayList<Group> max = network.getMaxHH();
-		ArrayList<Group> min = network.getMinHH();
-		ArrayList<Group> uni = network.getUniHH();
-		ArrayList<Group> pro = network.getProHH();
-		//ArrayList<Group> par = network.getParHH();
-		
-		if(network.isGraphic() && !chosen.isEmpty())
+		ObservableList<String> inputs = FXCollections.observableArrayList();
+		try 
 		{
-			Stage view = new Stage();
-			view.setTitle("Sequence: " + network.getSequence());
-			view.setResizable(true);
-			view.setMinWidth(790);
-			view.setMaxWidth(1200);
-			view.setMinHeight(420);
-			view.setMaxHeight(850);
-			view.initModality(Modality.WINDOW_MODAL);
-			view.initOwner(owner);
+			Scanner in = new Scanner(new File(input.getText()));
+			while(in.hasNextLine())
+				inputs.add(in.nextLine());
+			in.close();
+		} catch (FileNotFoundException e) {}
+		
+		ListView<String> list = new ListView<String>(inputs);
+		final Stage view = new Stage();
+		view.setTitle("Input List");
+		view.setResizable(true);
+		view.setMinWidth(420);
+		view.setMaxWidth(1200);
+		view.setMinHeight(420);
+		view.setMaxHeight(840);
+		view.initModality(Modality.WINDOW_MODAL);
+		view.initOwner(owner);
+		VBox v1 = new VBox(list);
 			
-			TilePane graphs = new TilePane();
-			graphs.setHgap(10);
-			graphs.setVgap(10);
-			graphs.setPrefWidth(780);
-			graphs.setPrefRows(2);
-			graphs.setPrefColumns(3);
-			
-			if(!max.isEmpty() && chosen.contains("Max"))
-				graphs.getChildren().add(max.get(max.size() - 1));
-			if(!min.isEmpty() && chosen.contains("Min"))
-				graphs.getChildren().add(min.get(min.size() - 1));
-			if(!uni.isEmpty() && chosen.contains("Uniform"))
-				graphs.getChildren().add(uni.get(uni.size() - 1));
-			if(!pro.isEmpty() && chosen.contains("Probabilistic"))
-				graphs.getChildren().add(pro.get(pro.size() - 1));
-			//if(!par.isEmpty() && chosen.contains("Parametric"))
-			//	graphs.getChildren().add(par.get(par.size() - 1));
-			
-			Scene display = new Scene(graphs);
-			view.setScene(display);
-			view.show();
-		}
-		input.setText(network.getOutput());
+		Scene display = new Scene(v1);
+		view.setScene(display);
+		view.sizeToScene();
+		view.show();
+		
+		list.getSelectionModel().selectedItemProperty().addListener((obs, oldValue, newValue) -> 	
+		{ 	
+			manualEntryGraphs(view, newValue, chosen, parameter);
+		});
 	}
 	
 	public static void main(String[] args)
